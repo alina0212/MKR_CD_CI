56f561d4e7357b0330df90ce0b61603f5643bfb6
alina <alkovryga@gmail.com>
2024-04-05 10:20:28 +0300

    Add main logic + compare function + test for compare

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+from typing import Set
+
+
 def read_file(file_path):
     with open(file_path, 'r') as file:
         return set(file.readlines())
@@ -5,12 +9,45 @@ def read_file(file_path):
 
 def write_to_file(file_path, lines):
     with open(file_path, 'w') as file:
-        file.writelines(lines)
+        file.writelines(list(lines))  # Convert set to list
+
+
+def compare_files(file1_path, file2_path) -> tuple[Set[str], Set[str]]:
+    """
+
+    :param file1_path:
+    :param file2_path:
+    :return:
+    """
+    file1_lines = read_file(file1_path)
+    file2_lines = read_file(file2_path)
+
+    # Find common lines
+    same_lines = set()
+    for line in file1_lines:
+        if line in file2_lines:
+            same_lines.add(line)
+
+    # Find lines that are different (present in only one file)
+    diff_lines = set()
+    for line in file1_lines:
+        if line not in file2_lines:
+            diff_lines.add(line)
+
+    for line in file2_lines:
+        if line not in file1_lines:
+            diff_lines.add(line)
+
+    return same_lines, diff_lines
 
 
 def main():
-    file1_path = "text1.txt"
-    file2_path = "file2.txt"
+    file1_path = os.path.join("text1.txt")
+    file2_path = os.path.join("text2.txt")
+    same_lines, diff_lines = compare_files(file1_path, file2_path)
+
+    write_to_file("same.txt", same_lines)
+    write_to_file("diff.txt", diff_lines)
 
 
 if __name__ == "__main__":
---
+++ test_compare_files.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+from main import compare_files
+
+
+@pytest.fixture
+def create_test_files():
+    file1_path = "test_file1.txt"
+    file2_path = "test_file2.txt"
+    with open(file1_path, 'w') as file1:
+        file1.write("line1\nline2\nline3\n")
+    with open(file2_path, 'w') as file2:
+        file2.write("line2\nline3\nline4\n")
+    yield file1_path, file2_path
+    os.remove(file1_path)
+    os.remove(file2_path)
+
+
+def test_compare_files(create_test_files):
+    file1_path, file2_path = create_test_files
+    same_lines, diff_lines = compare_files(file1_path, file2_path)
+    assert same_lines == {"line2\n", "line3\n"}
+    assert diff_lines == {"line1\n", "line4\n"}
